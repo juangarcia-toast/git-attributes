fcf8c8e07e9207676bf371dc1400e72b6371e5dd
Juan Garcia <juanm-mb@juan-mp.attlocal.net>
2019-02-12 23:11:38 -0500

    testing message

--- test.sql
+++ test.sql
@@ -1,22 +1,22 @@
 /*
 	$Id: skinnyTemplate.sql 42285 2017-08-08 16:47:48Z juanmg $
-	juanm-mb jljlk
+	$Author: juanm-mb $ jljlk
 	$Rev: 42288 $
-    test.sql
-	Tue Feb 12 23:10:31 EST 2019
-    juanm-mb   ---- asfd $AUthor: $
-    juanm-mb
+    $URL: test.sql$
+	$Date: Tue Feb 12 23:11:37 EST 2019 $
+    $Author: juanm-mb $   ---- asfd $AUthor: $
+    $Author: juanm-mb $
     ; asdfas asdf ; askdjfksdf ;24234
 
 ---
-	juanm-mb juanm-mb asdfjk outside of limits .;asldfaksldf alsjdf juanm-mb asdf sdf
-	Tue Feb 12 23:10:31 EST 2019
-    test.sql
+	$Author: juanm-mb $ $Author: juanm-mb $ asdfjk outside of limits .;asldfaksldf alsjdf $Author: juanm-mb $ asdf sdf
+	$Date: Tue Feb 12 23:11:37 EST 2019 $
+    $URL: test.sql$
     $Rev: 42288 $
     >Author: asdf > sadfl sdfl kajsd >Author: asdlfsd>
     colAuthor: asdf col sadfl sdfl kajsd colAuthor: asdlfsdcol
 */
 
-juanm-mb - juanm-mb
+$Author: juanm-mb $ - $Author: juanm-mb $
 
 asdf
