4c07618ef777a2f1c9b3fc96d5f356cb4b09f480
Juan Garcia <juanm-mb@juan-mp.attlocal.net>
2019-02-12 23:04:50 -0500

    testing message

--- test.sql
+++ test.sql
@@ -1,21 +1,20 @@
 /*
-	This Source Code is owned completely by Our Alter Ego Software (OAESoftware.com).
-	Any use of this source code is explicitly prohibited unless authorized specifically and in writing by OAE Software.
-	Copyright 2011-2016(c) OAE Software, LLC.
-*/
-
-/*
- asfsadfs fadjlf lsjdfj lkajsdflk jaskldfj-mb $: juanm-mb $
 	$Id: skinnyTemplate.sql 42285 2017-08-08 16:47:48Z juanmg $
+	replaced jljlk
 	$Rev: 42288 $
     $URL: test.sql $
 	$Date: Tue Feb 12 18:37:57 EST 2019 $
+    replaced   ---- asfd $AUthor: $
+    replaced
+    ; asdfas asdf ; askdjfksdf ;24234
 
 ---
- jasdfsd uanm-mb $ $Author: juanm-mb $
+	replaced $Author: 222 uanm-mb $ asdfjk outside of limits .;asldfaksldf alsjdf $Author: 333 23 $ asdf sdf
 	$Date: Tue Feb 12 18:37:57 EST 2019 $
     $URL: test.sql $
     $Rev: 42288 $
+    >Author: asdf > sadfl sdfl kajsd >Author: asdlfsd>
+    colAuthor: asdf col sadfl sdfl kajsd colAuthor: asdlfsdcol
 */
 
-set _scriptName:='$URL: svn://www.perfecttrax.com/eStratEx/trunk/crmsql/scripts/skinnyTemplate.sql $';
+replaced - $Author: a2344 $
