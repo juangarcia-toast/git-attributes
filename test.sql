dbbda587ec1bc2e5205b8535d8241f86f0baf233
Juan Garcia <juanm-mb@juan-mp.attlocal.net>
2019-02-12 23:15:14 -0500

    testing message

--- test.sql
+++ test.sql
@@ -1,22 +1,24 @@
 /*
 	$Id: skinnyTemplate.sql 42285 2017-08-08 16:47:48Z juanmg $
-	$Author: juanm-mb $ jljlk
+	$Author: asfsadfs fadjlf lsjdfj lkajsdflk jaskldfj-mb $ jljlk
 	$Rev: 42288 $
-    $URL: test.sql$
-	$Date: Tue Feb 12 23:11:37 EST 2019 $
-    $Author: juanm-mb $   ---- asfd $AUthor: $
-    $Author: juanm-mb $
+    $URL: test.sql $
+	$Date: Tue Feb 12 18:37:57 EST 2019 $
+    $Author$   ---- asfd $AUthor: $
+    $Author:$
     ; asdfas asdf ; askdjfksdf ;24234
 
 ---
-	$Author: juanm-mb $ $Author: juanm-mb $ asdfjk outside of limits .;asldfaksldf alsjdf $Author: juanm-mb $ asdf sdf
-	$Date: Tue Feb 12 23:11:37 EST 2019 $
-    $URL: test.sql$
+	$Author: 1111 ;uanm-mb $ $Author: 222 uanm-mb $ asdfjk outside of limits .;asldfaksldf alsjdf $Author: 333 23 $ asdf sdf
+	$Date: Tue Feb 12 18:37:57 EST 2019 $
+    $URL: test.sql $
     $Rev: 42288 $
     >Author: asdf > sadfl sdfl kajsd >Author: asdlfsd>
     colAuthor: asdf col sadfl sdfl kajsd colAuthor: asdlfsdcol
 */
 
-$Author: juanm-mb $ - $Author: juanm-mb $
+$Author: asdf 23 $ - $Author: a2344 $
+sdf
 
 asdf
+Tue Feb 12 18:37:57 EST 2019
