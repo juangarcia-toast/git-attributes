10ebe4d561f25652adf65f604714fa88f4f4f7b9
Juan Garcia <juanm-mb@juan-mp.attlocal.net>
2019-02-12 23:40:58 -0500

    asdf - props

--- test.sql
+++ test.sql
@@ -1,18 +1,18 @@
 /*
 	$Id: skinnyTemplate.sql 42285 2017-08-08 16:47:48Z juanmg $
 	$Author: juanm-mb $ jljlk
-	$Rev: 0a2985d904593bad0d9685e77e6347412cda3da3$
-    $URL: test.sql$
-	$Date: Tue Feb 12 23:36:58 EST 2019 $
+	$Rev: ac36f7570b33e57c4e257fa0b35a326ee6bcb828 $
+    $URL: test.sql $
+	$Date: Tue Feb 12 23:40:57 EST 2019 $
     $Author: juanm-mb $   ---- asfd $AUthor: $
     $Author: juanm-mb $
     ; asdfas asdf ; askdjfksdf ;24234
 
 ---
 	$Author: juanm-mb $ $Author: juanm-mb $ asdfjk outside of limits .;asldfaksldf alsjdf $Author: juanm-mb $ asdf sdf
-	$Date: Tue Feb 12 23:36:58 EST 2019 $
-    $URL: test.sql$
-    $Rev: 0a2985d904593bad0d9685e77e6347412cda3da3$
+	$Date: Tue Feb 12 23:40:57 EST 2019 $
+    $URL: test.sql $
+    $Rev: ac36f7570b33e57c4e257fa0b35a326ee6bcb828 $
     >Author: asdf > sadfl sdfl kajsd >Author: asdlfsd>
     colAuthor: asdf col sadfl sdfl kajsd colAuthor: asdlfsdcol
 */
